f80202d617029564b5372c55a2a9e3a4befe66a5
Meghan Denny <hello@nektro.net>
2021-08-27 19:42:25 -0700

    remove demo arg viewer

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,13 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.log.info("All your codebase are belong to us.", .{});
 
+    //
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     var arena = std.heap.ArenaAllocator.init(&gpa.allocator);
     defer arena.deinit();
     const alloc = &arena.allocator;
 
-    const proc_args = try std.process.argsAlloc(alloc);
-    const args = proc_args;
 
-    for (args) |item, i| {
-        std.log.info("{d}: {s}", .{ i, item });
-    }
 }
